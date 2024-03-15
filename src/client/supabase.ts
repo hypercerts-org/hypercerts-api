67e651e9942c04415725ee4f6599a63be2778355
bitbeckers <code@bitbeckers.com>
2024-03-15 14:00:43 +0100

    feat(stitch): get matching attestations and count

--- src/client/supabase.ts
+++ src/client/supabase.ts
@@ -1,6 +1,6 @@
 import {supabaseApiKey, supabaseUrl} from "@/utils/constants";
 import {createClient} from "@supabase/supabase-js";
-import {Database} from "@/types/database-generated.types";
+import {Database} from "@/types/supabase";
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient<Database>(
