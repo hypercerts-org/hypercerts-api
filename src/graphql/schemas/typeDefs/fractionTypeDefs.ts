4c122caa9a0c07cc681c2e951e02b5308dabb3f0
bitbeckers <code@bitbeckers.com>
2024-06-18 20:02:20 +0200

    chore(docs): field annotations fractionTypeDefs

--- src/graphql/schemas/typeDefs/fractionTypeDefs.ts
+++ src/graphql/schemas/typeDefs/fractionTypeDefs.ts
@@ -7,21 +7,25 @@ import {Metadata} from "./metadataTypeDefs.js";
 
 @ObjectType()
 class Fraction extends BasicTypeDef {
-    @Field({nullable: true})
+    @Field({nullable: true, description: "Address of the owner of the fractions"})
     owner_address?: string;
-    @Field((_) => EthBigInt, {nullable: true})
+    @Field((_) => EthBigInt, {nullable: true, "Units held by the fraction"})
     units?: bigint | number;
     @Field((_) => GraphQLBigInt, {nullable: true})
     creation_block_timestamp?: bigint | number;
     @Field((_) => GraphQLBigInt, {nullable: true})
     last_block_update_timestamp?: bigint | number;
-    @Field((_) => ID, {name: "fraction_id", nullable: true})
+    @Field((_) => ID, {
+        name: "fraction_id",
+        nullable: true,
+        description: "The ID of the fraction concatenated from the chain ID, contract address, and token ID"
+    })
     hypercert_id?: string;
 
     // Resolved fields
-    @Field(() => GetOrdersResponse, {nullable: true})
+    @Field(() => GetOrdersResponse, {nullable: true, description: "Marketplace orders related to this fraction"})
     orders?: GetOrdersResponse;
-    @Field(() => Metadata, {nullable: true})
+    @Field(() => Metadata, {nullable: true, description: "The metadata for the fraction"})
     metadata?: Metadata;
 }
 
