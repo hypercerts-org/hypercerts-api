9d91a22bc9cc2a0142877f63bb18e2eeba4a6681
bitbeckers <code@bitbeckers.com>
2024-06-19 12:32:05 +0200

    feat(records): add to graph with filter and sort

--- src/graphql/schemas/resolvers/composed.ts
+++ src/graphql/schemas/resolvers/composed.ts
@@ -1,19 +1,21 @@
-import { HypercertResolver } from "./hypercertResolver.js";
-import { MetadataResolver } from "./metadataResolver.js";
-import { ContractResolver } from "./contractResolver.js";
-import { FractionResolver } from "./fractionResolver.js";
-import { AttestationResolver } from "./attestationResolver.js";
-import { AttestationSchemaResolver } from "./attestationSchemaResolver.js";
-import { OrderResolver } from "./orderResolver.js";
-import { CollectionResolver } from "./collectionResolver.js";
+import {HypercertResolver} from "./hypercertResolver.js";
+import {MetadataResolver} from "./metadataResolver.js";
+import {ContractResolver} from "./contractResolver.js";
+import {FractionResolver} from "./fractionResolver.js";
+import {AttestationResolver} from "./attestationResolver.js";
+import {AttestationSchemaResolver} from "./attestationSchemaResolver.js";
+import {OrderResolver} from "./orderResolver.js";
+import {CollectionResolver} from "./collectionResolver.js";
+import {AllowlistRecordResolver} from "./allowlistRecordResolver.js";
 
 export const resolvers = [
-  ContractResolver,
-  FractionResolver,
-  MetadataResolver,
-  HypercertResolver,
-  AttestationResolver,
-  AttestationSchemaResolver,
-  OrderResolver,
-  CollectionResolver
+    ContractResolver,
+    FractionResolver,
+    MetadataResolver,
+    HypercertResolver,
+    AttestationResolver,
+    AttestationSchemaResolver,
+    OrderResolver,
+    CollectionResolver,
+    AllowlistRecordResolver
 ] as const;
