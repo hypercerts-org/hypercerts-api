ff2e19294344f795e3532f57de3e9bfd47e8ae28
jipstavenuiter <jipstavenuiter@gmail.com>
2024-07-07 13:47:39 +0200

    (feat): validate if total number of units in allowlist is 10**18

--- src/utils/parseAndValidateMerkleTreeDump.ts
+++ src/utils/parseAndValidateMerkleTreeDump.ts
@@ -1,30 +1,56 @@
-import {validateAllowlist} from "@hypercerts-org/sdk";
-import {parseMerkleTree} from "./parseMerkleTree.js";
-import {CreateAllowListRequest} from "../controllers/AllowListController.js";
+import { validateAllowlist } from "@hypercerts-org/sdk";
+import { parseMerkleTree } from "./parseMerkleTree.js";
+import { CreateAllowListRequest } from "../controllers/AllowListController.js";
+import { parseEther } from "viem";
 
 export const parseAndValidateMerkleTree = (request: CreateAllowListRequest) => {
+  const { allowList, totalUnits } = request;
 
-    const {allowList, totalUnits} = request;
+  const _merkleTree = parseMerkleTree(allowList);
 
-    const _merkleTree = parseMerkleTree(allowList)
+  if (!_merkleTree) {
+    return {
+      data: _merkleTree,
+      valid: false,
+      errors: {
+        allowListData: "Data could not be parsed to OpenZeppelin MerkleTree",
+      },
+    };
+  }
 
-    if (!_merkleTree) {
-        return {data: _merkleTree, valid: false, errors: {allowListData: "Data could not be parsed to OpenZeppelin MerkleTree"}};
-    }
+  const merkleEntries = Array.from(_merkleTree.entries()).map(
+    (entry) => entry[1],
+  );
 
-    const merkleEntries = Array.from(_merkleTree.entries()).map((entry) => entry[1]);
+  const allowListEntries = merkleEntries
+    .map((row) => ({ address: row[0], units: BigInt(row[1]) }))
+    .flatMap((entry) => (entry ? [entry] : []));
 
-    const allowListEntries = merkleEntries
-        .map((row) =>
-            ({address: row[0], units: BigInt(row[1])})
-        )
-        .flatMap((entry) => (entry ? [entry] : []));
+  const totalUnitsInEntries = allowListEntries.reduce(
+    (acc, entry) => acc + entry.units,
+    BigInt(0),
+  );
 
+  if (totalUnitsInEntries !== BigInt(totalUnits)) {
+    return {
+      data: _merkleTree,
+      valid: false,
+      errors: {
+        totalUnits:
+          "Total units do not match the sum of units in the allowlist",
+      },
+    };
+  }
 
-    return validateAllowlist(
-        allowListEntries,
-        BigInt(totalUnits)
-    );
+  if (totalUnitsInEntries !== parseEther("1")) {
+    return {
+      data: _merkleTree,
+      valid: false,
+      errors: {
+        totalUnits: "Total units should amount to 1 eth in wei (1e18) units",
+      },
+    };
+  }
 
-}
-
+  return validateAllowlist(allowListEntries, BigInt(totalUnits));
+};
