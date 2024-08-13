cfa3b766e021a430b2a1a037939148c61c125e7f
jipstavenuiter <jipstavenuiter@gmail.com>
2024-08-13 19:50:47 +0200

    (fix): add missing migration

---
+++ supabase/migrations/20240813175015_add_rls_policy_for_orders.sql
@@ -0,0 +1,9 @@
+alter table "public"."marketplace_orders"
+    enable row level security;
+
+create policy "Enable read access for all users"
+    on "public"."marketplace_orders"
+    as permissive
+    for select
+    to public
+    using (true);
