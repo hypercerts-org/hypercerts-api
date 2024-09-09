95f8fc7c111c75448668f78fe0591e56424ee87d
bitbeckers <code@bitbeckers.com>
2024-09-09 14:52:54 +0200

    fix(types): counts as ints not floats

--- src/graphql/schemas/typeDefs/baseTypes/hypercertBaseType.ts
+++ src/graphql/schemas/typeDefs/baseTypes/hypercertBaseType.ts
@@ -1,4 +1,4 @@
-import { Field, ID, ObjectType } from "type-graphql";
+import { Field, ID, ObjectType, Int } from "type-graphql";
 import { BasicTypeDef } from "./basicTypeDef.js";
 import { EthBigInt } from "../../../scalars/ethBigInt.js";
 import { Metadata } from "../metadataTypeDefs.js";
@@ -52,13 +52,13 @@ class HypercertBaseType extends BasicTypeDef {
   @Field(() => EthBigInt, { nullable: true })
   last_update_block_timestamp?: bigint | number | string;
 
-  @Field({
+  @Field(() => Int, {
     nullable: true,
     description: "Count of attestations referencing this hypercert",
   })
   attestations_count?: number;
 
-  @Field({
+  @Field(() => Int, {
     nullable: true,
     description: "Count of sales of fractions that belong to this hypercert",
   })
