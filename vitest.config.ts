8141388242c265da3f21ae5e0847e00c673ab1ef
bitbeckers <code@bitbeckers.com>
2025-01-02 22:13:57 +0100

    chore(coverage): add coverage reporting and ci
    Adds vitest coverage reporting and CI action

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,32 @@
 import { resolve } from "node:path";
-import { defineConfig } from "vitest/config";
+import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     setupFiles: ["./test/setup-env.ts"],
-    exclude: ["./lib", "node_modules"],
+    exclude: [...configDefaults.exclude, "./lib/**/*"],
+    coverage: {
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ["text", "json-summary", "json"],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
+      thresholds: {
+        lines: 15,
+        branches: 54,
+        functions: 49,
+        statements: 15,
+      },
+      include: ["src/**/*.ts"],
+      exclude: [
+        ...(configDefaults.coverage.exclude as string[]),
+        "**/*.types.ts",
+        "**/types.ts",
+        "src/__generated__/**/*",
+        "src/graphql/**/*",
+        "src/types/**/*",
+        "./lib/**/*",
+      ],
+    },
   },
   resolve: {
     alias: [{ find: "@", replacement: resolve(__dirname, "./src") }],
