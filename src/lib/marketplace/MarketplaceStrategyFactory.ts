a4b32b81c3752765ec3e64f9e34a85e6f318aa29
jipstavenuiter <jipstavenuiter@gmail.com>
2025-04-17 11:35:52 -0400

    fix: build

--- src/lib/marketplace/MarketplaceStrategyFactory.ts
+++ src/lib/marketplace/MarketplaceStrategyFactory.ts
@@ -7,16 +7,20 @@ import EOACreateOrderStrategy from "./EOACreateOrderStrategy.js";
 import MultisigCreateOrderStrategy from "./MultisigCreateOrderStrategy.js";
 import { container } from "tsyringe";
 
-export function createMarketplaceStrategy(
+export function createMarketplaceStrategy({
   type,
-  ...request: MultisigCreateOrderRequest | EOACreateOrderRequest,
-): MarketplaceStrategy {
+  ...request
+}: MultisigCreateOrderRequest | EOACreateOrderRequest): MarketplaceStrategy {
   switch (type) {
     case "eoa": {
-      return container.resolve(EOACreateOrderStrategy).initialize(request as Omit<EOACreateOrderRequest, "type">);
+      return container
+        .resolve(EOACreateOrderStrategy)
+        .initialize(request as Omit<EOACreateOrderRequest, "type">);
     }
     case "multisig": {
-      return container.resolve(MultisigCreateOrderStrategy).initialize(request as Omit<MultisigCreateOrderRequest, "type">);
+      return container
+        .resolve(MultisigCreateOrderStrategy)
+        .initialize(request as Omit<MultisigCreateOrderRequest, "type">);
     }
     default:
       throw new Error("Invalid marketplace request type");
