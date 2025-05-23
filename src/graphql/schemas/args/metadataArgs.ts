af991d1f35e5e14f9df42aa5d60fc179dc4e6525
jipstavenuiter <jipstavenuiter@gmail.com>
2025-05-23 18:37:50 -0400

    feat(metadataArgs): add hypercert reference to Metadata entity arguments
    - Introduced a new 'hypercert' field in the Metadata entity arguments.
    - The 'hypercert' field is defined as an ID type and references the Hypercert entity and its fields.

--- src/graphql/schemas/args/metadataArgs.ts
+++ src/graphql/schemas/args/metadataArgs.ts
@@ -2,10 +2,18 @@ import { ArgsType } from "type-graphql";
 import { BaseQueryArgs } from "../../../lib/graphql/BaseQueryArgs.js";
 import { createEntityArgs } from "../../../lib/graphql/createEntityArgs.js";
 import { WhereFieldDefinitions } from "../../../lib/graphql/whereFieldDefinitions.js";
+import { EntityTypeDefs } from "../typeDefs/typeDefs.js";
 
 const { WhereInput: MetadataWhereInput, SortOptions: MetadataSortOptions } =
   createEntityArgs("Metadata", {
     ...WhereFieldDefinitions.Metadata.fields,
+    hypercert: {
+      type: "id",
+      references: {
+        entity: EntityTypeDefs.Hypercert,
+        fields: WhereFieldDefinitions.Hypercert.fields,
+      },
+    },
   });
 
 @ArgsType()
